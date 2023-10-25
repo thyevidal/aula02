d749e4e8967b033a66aaa108157c74ec59b340ac
thye <thyevidal@gmail.com>
2023-10-25 20:31:05 -0300

    .

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Clock(props){
+    const element = (
+    <div><h1>Hora: {props.date.toLocaleTimeString()} </h1></div>
+    )
+    return element;
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function tick(){
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+    <Clock date={new Date()} />
+)
+}
+
